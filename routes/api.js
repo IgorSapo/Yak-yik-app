8c18a02b7e55633d14befd1aaaba680b7ed81616
IgorS <amsterdam1611@gmail.com>
2017-05-22 23:31:14 +0400

    Folder structure fixed

---
+++ routes/api.js
@@ -0,0 +1,115 @@
+var express = require('express');
+var router = express.Router();
+var ZoneController = require('../controllers/ZoneController');
+var controllers = require('../controllers');
+
+router.get('/:resource', function(req, res, next) {
+
+  var resource = req.params.resource;
+  var controller = controllers[resource];
+
+  if (!controller) {
+    res.json({
+      confirmation: 'fail',
+      message: 'Invalid resource request: ' + resource,
+    });
+    return;
+  };
+
+  controller.find(req.query, function(err, results) {
+    if (err) {
+      res.json({
+        confirmation: 'fail',
+        message: err
+      });
+
+      return;
+    }
+
+    res.json({
+      confirmation: 'success',
+      results: results
+    });
+  })
+
+  // if (resource === 'zone') {
+  //   ZoneController.find(req.query, function(err, results) {
+  //     if (err) {
+  //       res.json({
+  //         confirmation: 'fail',
+  //         message: err
+  //       });
+
+  //       return;
+  //     }
+
+  //     res.json({
+  //       confirmation: 'success',
+  //       results: results
+  //     });
+  //   })
+  // }
+
+});
+
+router.get('/:resource/:id', function(req, res, next) {
+  var resource = req.params.resource;
+  var id = req.params.id;
+
+  var controller = controllers[resource];
+
+  if (!controller) {
+    res.json({
+      confirmation: 'fail',
+      message: 'Invalid resource request: ' + resource,
+    });
+    return;
+  };
+
+  controller.findById(id, function(err, result) {
+    if (err) {
+      res.json({
+        confirmation: 'fail',
+        message: 'Not found'
+      })
+      return;
+    }
+    res.json({
+      confirmation: 'success',
+      result: result
+    })
+  })
+
+})
+
+router.post('/:resource', function(req, res, next) {
+  var resource = req.params.resource;
+
+  var controller = controllers[resource];
+
+  if (!controller) {
+    res.json({
+      confirmation: 'fail',
+      message: 'Invalid resource request: ' + resource,
+    });
+    return;
+  };
+
+  controller.create(req.body, function(err, result) {
+    if (err) {
+      res.json({
+        confirmation: 'failure',
+        ressage: err
+      });
+      return;
+    }
+
+    res.json({
+      confirmation: 'success',
+      result: result
+    })
+  })
+
+})
+
+module.exports = router;
